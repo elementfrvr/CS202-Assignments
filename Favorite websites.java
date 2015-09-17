a9822309be2ec81e03e4b7716043229aacb6f00c
elementfrvr <dakota.mitchem@siu.edu>
2015-09-17 11:49:55 -0500

    Create Favorite websites.java

---
+++ Favorite websites.java
@@ -0,0 +1,14 @@
+// Dakota Mitchem
+// CS 202 Section 002
+// Assignment 2
+// Goal: Print a list of favorite websites and URLs
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Prints the requested information
+System.out.println("Top Websites:\nReddit: (www.reddit.com)\nJalopnik: (www.jalopnik.com)\nFacebook: (www.facebook.com)\nReverb: "
+		+ "(www.reverb.com)\nPremier Guitar: (www.premierguitar.com)");
+	}
+
+}
