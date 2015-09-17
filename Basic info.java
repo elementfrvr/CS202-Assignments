9955cfc81645d6f6145160f5df19f217fb8f1f9e
elementfrvr <dakota.mitchem@siu.edu>
2015-09-17 11:49:14 -0500

    Create Basic info.java

---
+++ Basic info.java
@@ -0,0 +1,14 @@
+// Dakota Mitchem
+// CS 202 Section 002
+// Assignment 1
+// Goal: Write an application to print and label basic information about me 
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Prints the requested information
+System.out.println("Name: Dakota Mitchem\nBirthday: January 13th, 1996\nHobbies: Playing guitar and listening to music"
+		+ "\nFavorite Book: The Last Lecture\nFavorite Movie: Forrest Gump");
+	}
+
+}
